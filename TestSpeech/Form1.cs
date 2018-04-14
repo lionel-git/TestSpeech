aa03f5c3ae47ec518e36f2ba2b386322575a1162
Lionel Desorme <lionel.desorme@free.fr>
2018-04-14 14:27:53 +0200

    Test user save

--- TestSpeech/Form1.cs
+++ TestSpeech/Form1.cs
@@ -57,6 +57,7 @@ namespace TestSpeech
                     text = text.Substring(richTextBox1.SelectionStart, text.Length - richTextBox1.SelectionStart);
             }
 
+
             if (string.IsNullOrEmpty(text))
             {
                 MessageBox.Show("No text selected", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
