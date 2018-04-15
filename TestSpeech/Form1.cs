96ac1367e4ff1e07471750721729f20f94756c08
Lionel Desorme <lionel.desorme@free.fr>
2018-04-15 09:37:23 +0200

    remove ref

--- TestSpeech/Form1.cs
+++ TestSpeech/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Speech.Synthesis;
 using System.IO;
 using Microsoft.Office.Interop.Word;
 using System.Reflection;
